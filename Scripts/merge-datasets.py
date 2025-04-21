4ef447cb8dfea6c0c696998ef349250d3c07a086
Bdem1rel <berkedel05@gmail.com>
2025-04-21 18:58:36 +0300

    Update merge-datasets.py script and add the merged dataset

--- Scripts/merge-datasets.py
+++ Scripts/merge-datasets.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-library_df = pd.read_csv("..\Raw_Data\PLS_FY22_AE_pud22i.csv")
+library_df = pd.read_csv("../Datasets/Raw_Data/PLS_FY22_AE_pud22i.csv", encoding='ISO-8859-1')
 
-library_df["ZIP_CODE"] = (
+library_df["ZIP-CODE"] = (
     library_df["ZIP"]
     .astype(str)
     .str.zfill(5)
@@ -11,26 +11,22 @@ library_df["ZIP_CODE"] = (
 library_df = library_df.drop(columns=['ZIP', 'ZIP4', 'ZIP_M', 'ZIP4_M'])
 
 
-census_df = pd.read_csv("..\Raw_Data\census_acs_2022.csv")
+census_df = pd.read_csv("../Datasets/Raw_Data/census_acs_2022.csv")
 
-census_df["ZIP_CODE"] = (
+census_df["ZIP-CODE"] = (
     census_df["ZIP_CODE"]
-    .str.extract(r'(\d{5})')[0]
+    .astype(str)
+    .str.zfill(5)
 )
 
-census_df = census_df.drop(columns=['NAME'])
+census_df = census_df.drop(columns=['NAME', 'ZIP_CODE'])
 
 merged_df = pd.merge(
     library_df,
     census_df,
-    on='ZIP_CODE',
+    on='ZIP-CODE',
     how='inner',
     suffixes=('_LIB', '_CENSUS')
 )
 
-merged_df.to_csv("merged_data.csv", index=False)
-
-
-
-
-
+merged_df.to_csv("../Datasets/Merged_Data/merged_data.csv", index=False)
