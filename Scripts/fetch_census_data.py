e3a83e0f705ef6ca5a222dfa4be385f339cee80b
Bdem1rel <berkedel05@gmail.com>
2025-04-22 22:09:47 +0300

    Update fetch_census_data.py script

--- Scripts/fetch_census_data.py
+++ Scripts/fetch_census_data.py
@@ -7,7 +7,7 @@ c = Census(API_KEY)
 
 # Fetch data for all ZIP Code Tabulation Areas (ZCTA)
 data = c.acs5.get(
-    ("NAME", "B19013_001E", "B15003_022E", "B01003_001E", "B23025_005E"),
+    ("NAME", "B19013_001E", "B15003_022E", "B23025_005E"),
     {"for": "zip code tabulation area:*"}  # "*" fetches all ZCTAs
 )
 
@@ -18,7 +18,6 @@ census_df = pd.DataFrame(data)
 census_df.rename(columns={
     "B19013_001E": "MEDIAN_INCOME",
     "B15003_022E": "BACHELORS_PERCENT",
-    "B01003_001E": "POPULATION",
     "B23025_005E": "UNEMPLOYMENT_RATE",
     "zip code tabulation area": "ZIP_CODE"
 }, inplace=True)
